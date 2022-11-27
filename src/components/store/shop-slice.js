917ff385e140b7049c3a5c391bbdc690794f3554
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-28 01:47:18 +0300

    add more shopping items

--- src/components/store/shop-slice.js
+++ src/components/store/shop-slice.js
@@ -6,15 +6,27 @@ const shopSlice = createSlice({
     items: [
       {
         id: "m1",
-        title: "Headphones",
+        title: "Redux Headphones",
         description: "Listen to your favourite music with ease",
         price: 6,
       },
       {
         id: "m2",
-        title: "Power Bank",
+        title: "Redux Bank",
         description: "Keep your phone alive all the time, anywhere",
         price: 18,
+      },
+      {
+        id: "m3",
+        title: "Redux Case",
+        description: "Don't worry about breaking your phone ever again",
+        price: 5,
+      },
+      {
+        id: "m4",
+        title: "Redux Wallet",
+        description: "Track your wallet with our built in wallet GPS",
+        price: 35,
       },
     ],
   },
